3c6a8a0363239db055855746cf84ae83e57c6ee8
prater12 <praterzwolf@gmail.com>
2024-03-16 20:15:52 +0530

    Fixed team.getDeployPoint

--- engine/team.py
+++ engine/team.py
@@ -207,7 +207,7 @@ class Team:
         return self.__wood
 
     def getDeployPoint(self):
-        return (self.rect.x // 20, self.rect.y // 20)
+        return (self.base[0], self.base[1])
 
     def getDimensionX(self):
         return self.__myGame._Game__dim[0]
